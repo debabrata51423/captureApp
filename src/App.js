3ee074eee2814ec942f8d2be467a29caa61b97ae
debabrata51423 <34102204+debabrata51423@users.noreply.github.com>
2022-02-14 00:59:02 +0530

    Styled Component Intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import AboutSection from "./components/AboutSection";
+import GlobalStyle from "./components/GlobalStyle";
+
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
     </div>
   );
